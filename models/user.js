3409d0b73770d6b98fe61aef5603411035b37436
BaileyBeck <baileybk2@gmail.com>
2024-07-08 11:25:34 -0400

    country schema complete

--- models/user.js
+++ models/user.js
@@ -1,4 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+const countrySchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  pointOfEntry: {
+    type: String,
+  },
+  purpose: {
+    type: String,
+    enum: ["Leisure", "Business", "Education", "Other"],
+  },
+  notes: {
+    type: String,
+  },
+  timesVisited: {
+    type: String,
+    enum: ["1", "2", "3", "4", "5", "5 +"],
+  },
+});
 
 const userSchema = mongoose.Schema({
   username: {
@@ -9,8 +34,9 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  countries: [countrySchema],
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
