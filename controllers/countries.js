05c341cdf1f0965c2c4c87687492293af327e7e0
BaileyBeck <baileybk2@gmail.com>
2024-07-09 10:58:05 -0400

    built create functionality

--- controllers/countries.js
+++ controllers/countries.js
@@ -16,4 +16,16 @@ router.get("/new", async (req, res) => {
   res.render("countries/new.ejs");
 });
 
+router.post("/", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    currentUser.countries.push(req.body);
+    await currentUser.save();
+    res.redirect(`/users/${currentUser._id}/countries`);
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
+
 module.exports = router;
